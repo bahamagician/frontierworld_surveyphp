baf84ef3e50706fe071ecc7ad3082278af1656ab
SC Graphics <scgraphics@SCs-iMac.local>
2018-12-27 17:00:23 -0500

    add db connection info for my local env

--- includes/pdo_connect.php
+++ includes/pdo_connect.php
@@ -1,5 +1,5 @@
 <?php
-$dsn = 'mysql:host=localhost;dbname=frontierworld_survey';
+$dsn = 'mysql:host=127.0.0.1;dbname=frontierworld_survey';
 
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // highly recommended
@@ -8,4 +8,4 @@ $options = [
   //https://gist.github.com/adrian-enspired/385c6830ba2932bc36a2
   // learn more here: https://phpdelusions.net/pdo#errors
 
-$db = new PDO($dsn, 'root', 'root'); //these credntials are for MAMP
+$db = new PDO($dsn, 'root', ''); //these credntials are for MAMP
